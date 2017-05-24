90f7ea488316b54381f1addd3e9017c3ed3b44ce
xuebinliu <xuebinliu@dengtacj.com>
2017-05-24 17:35:53 +0800

    添加随机头像

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,6 +1,7 @@
 
 //获取应用实例
 var common = require('../../utils/common.js');
+var generater = require('../../utils/generater.js');
 var Bmob=require("../../utils/bmob.js");
 var app = getApp()
 var that
@@ -55,14 +56,18 @@ Page({
     var key = app.globalData.user_id
     if (key) {
       wx.showActionSheet({
-        itemList: ['相册', '拍照'],
+        itemList: ['随机', '相册', '拍照'],
         success: function (res) {
           if (!res.cancel) {
             var sourceType = [];
             if (res.tapIndex == 0) {
+              // 随机
+              var avatar_url = generater.generateAvatar();
+              that.modifyUserAvatar(avatar_url)
+              return
+            } else if (res.tapIndex == 1) {
               sourceType = ['album']//从相册选择
-            }
-            else if (res.tapIndex == 1) {
+            } else if (res.tapIndex == 2) {
               sourceType = ['camera']//拍照
             }
 
@@ -71,61 +76,7 @@ Page({
               sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
               sourceType: sourceType, // 可以指定来源是相册还是相机，默认二者都有
               success: function (imageResult) {
-                // 开始上传
-                that.setData({
-                  uploadingImg: true
-                })
-                var tempFilePaths = imageResult.tempFilePaths;
-                if (tempFilePaths.length > 0) {
-                  var name = tempFilePaths;
-                  var file = new Bmob.File(name, tempFilePaths);
-                  // 保存到服务器
-                  file.save().then(function (resu) {
-                    // 上传完成
-                    wx.setStorageSync('my_avatar', resu.url());
-                    that.setData({
-                      uploadingImg: false
-                    });
-
-                    var newImge = resu.url();
-                    wx.getStorage({
-                      key: 'user_openid',
-                      success: function (openid) {
-                        // 保存到用户属性
-                        var openid = openid.data
-                        var user = Bmob.User.logIn(openid, openid, {
-                          success: function (users) {
-                            users.set('userPic', newImge);
-                            users.save(null, {
-                              success: function (user) {
-                                that.setData({
-                                  userImg: newImge
-                                })
-                                common.dataLoading("修改头像成功", "success");
-                              },                
-                            });
-                          },
-
-                          error: function (error) {
-                            console.log('error', error)
-                            common.dataLoading("修改头像失败", "loading");
-                          }
-                        });
-                      }, 
-                      fail: function(error) {
-                        // 查找openid失败
-                        common.dataLoading("修改头像失败", "loading");
-                        console.log(error);
-                      }
-                    })
-                  }, function (error) {
-                    that.setData({
-                      uploadingImg: false
-                    })
-                    common.dataLoading("修改头像失败", "loading");
-                    console.log(error);
-                  })
-                }
+                that.saveAvatar(imageResult)
               }
             })
           }
@@ -154,8 +105,68 @@ Page({
     })
   },
 
+  saveAvatar: function (imageResult) {
+    // 开始上传
+    that.setData({
+      uploadingImg: true
+    })
+    var tempFilePaths = imageResult.tempFilePaths;
+    if (tempFilePaths.length > 0) {
+      var name = tempFilePaths;
+      var file = new Bmob.File(name, tempFilePaths);
+      // 保存到服务器
+      file.save().then(function (resu) {
+        that.modifyUserAvatar(resu.url())
+      }, function (error) {
+        that.setData({
+          uploadingImg: false
+        })
+        common.dataLoading("修改头像失败", "loading");
+        console.log(error);
+      })
+    }
+  },
+
+  modifyUserAvatar: function(url) {
+    wx.setStorageSync('my_avatar', url);
+    that.setData({
+      uploadingImg: false
+    });
+
+    wx.getStorage({
+      key: 'user_openid',
+      success: function (openid) {
+        // 保存到用户属性
+        var openid = openid.data
+        var user = Bmob.User.logIn(openid, openid, {
+          success: function (users) {
+            users.set('userPic', url);
+            users.save(null, {
+              success: function (user) {
+                that.setData({
+                  userImg: url
+                })
+                common.dataLoading("修改头像成功", "success");
+              },
+            });
+          },
+
+          error: function (error) {
+            console.log('error', error)
+            common.dataLoading("修改头像失败", "loading");
+          }
+        });
+      },
+      fail: function (error) {
+        // 查找openid失败
+        common.dataLoading("修改头像失败", "loading");
+        console.log(error);
+      }
+    })
+  },
+
   //修改昵称
-  modyfiNick:function(e){
+  modyfiNick: function (e) {
     that.setData({
       isdisabled:true,
       modifyLoading:true
